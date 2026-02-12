c479bc50eff57b34d525f608baa7d901afbe9256
Liam Wright <28269309+LiamWright@users.noreply.github.com>
2026-02-12 12:24:01 +0000

    PV2-3834 - Updated handler with GSLShortCoursesEarningEvent

---
+++ src/SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService/Handlers/ShortCoursesEarningEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using NServiceBus;
+using SFA.DAS.Payments.Application.Infrastructure.Logging;
+using SFA.DAS.Payments.EarningEvents.Messages.Events;
+using SFA.DAS.Payments.RequiredPayments.RequiredPaymentsService.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Payments.RequiredPayments.RequiredPaymentsProxyService.Handlers
+{
+    public class ShortCoursesEarningEventHandler : IHandleMessages<GSLShortCourseEarningsEvent>
+    {
+        private readonly IActorProxyFactory proxyFactory;
+        private readonly IPaymentLogger paymentLogger;
+
+        public ShortCoursesEarningEventHandler(IActorProxyFactory proxyFactory, IPaymentLogger paymentLogger)
+        {
+            this.proxyFactory = proxyFactory;
+            this.paymentLogger = paymentLogger;
+        }
+        public async Task Handle(GSLShortCourseEarningsEvent message, IMessageHandlerContext context)
+        {
+            paymentLogger.LogInfo($"Processing GSLShortCourseEarningsEvent, UKPRN: {message.Ukprn}, JobId: {message.JobId}, Period: {message.CollectionPeriod}, ILR: {message.IlrSubmissionDateTime}");
+
+            var actorId = new ActorId(message.Ukprn.ToString());
+            var actor = proxyFactory.CreateActorProxy<IRequiredPaymentsService>(new Uri("fabric:/SFA.DAS.Payments.RequiredPayments.ServiceFabric/RequiredPaymentsService"), actorId);
+            var removedAims = await actor.HandleShortCoursesEarningEvent(message, CancellationToken.None).ConfigureAwait(false);
+            paymentLogger.LogInfo($"Finished GSLShortCourseEarningsEvent. UKPRN: {message.Ukprn}, JobId: {message.JobId}, Period: {message.CollectionPeriod}, ILR: {message.IlrSubmissionDateTime}");
+        }
+    }
+}
