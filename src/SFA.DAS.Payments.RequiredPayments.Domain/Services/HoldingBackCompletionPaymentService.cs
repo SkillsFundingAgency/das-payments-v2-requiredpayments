fc6cff6c42a274f15a25a1d8aec151e232ec1140
rakib-khan-dfe-payment <rakib.khan@education.gov.uk>
2026-09-15 12:41:14 +0000

    PV2-4274 Decouple Completion Payments from Co‑Investment Collection for Completions on or after 1 August 2026

--- src/SFA.DAS.Payments.RequiredPayments.Domain/Services/HoldingBackCompletionPaymentService.cs
+++ src/SFA.DAS.Payments.RequiredPayments.Domain/Services/HoldingBackCompletionPaymentService.cs
@@ -1,13 +1,21 @@
-﻿using SFA.DAS.Payments.Model.Core;
+﻿using System;
+using SFA.DAS.Payments.Model.Core;
 
 namespace SFA.DAS.Payments.RequiredPayments.Domain.Services
 {
     public class HoldingBackCompletionPaymentService : IHoldingBackCompletionPaymentService
     {
+        private static readonly DateTime PolicyChangeEffectiveDate = new DateTime(2026, 8, 1);
+
         public bool ShouldHoldBackCompletionPayment(decimal expectedContribution, PriceEpisode priceEpisode)
         {
-            var reportedContribution = priceEpisode.EmployerContribution ?? 0;
-            var completionHoldBackExemptionCode = priceEpisode.CompletionHoldBackExemptionCode ?? 0;
+            if (priceEpisode?.ActualEndDate != null && priceEpisode.ActualEndDate >= PolicyChangeEffectiveDate)
+            {
+                return false;
+            }
+
+            var reportedContribution = priceEpisode?.EmployerContribution ?? 0;
+            var completionHoldBackExemptionCode = priceEpisode?.CompletionHoldBackExemptionCode ?? 0;
 
             if (completionHoldBackExemptionCode > 0)
                 return false;
